5e839bc624b221cb17964568c56e3dcb9eba5ca8
Tawnee Harris <tawneeh@icloud.com>
2021-03-02 12:12:59 -0800

    attempt to display Graph component
    Co-authored-by: Chelsea Becker <cschweig2@gmail.com>
    Co-authored-by: Kevin Kirkley <kevinmkirkley@gmail.com>
    Co-authored-by: Randel Moore <Randel.C.Moore@gmail.com>

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { makeApiCall } from './../actions/';
+import Graph from './Graph';
 
 class Weather extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  // constructor(props) {
+  //   super(props);
+  // }
 
   componentDidMount() {
     const { dispatch } = this.props;
@@ -13,33 +14,38 @@ class Weather extends React.Component {
   }
 
   render() {
-
+    let displayGraph = null;
+    const handleDateTimeConversion = (dt) => {
+      const milliseconds = dt * 1000;
+      const dateObject = new Date(milliseconds);
+      const humanDateFormat = dateObject.toLocaleString("en-US", { weekday: "long", hour: "numeric" })
+      return humanDateFormat;
+    }
     const { error, isLoading, weatherData } = this.props;
-    console.log(typeof (weatherData));
     if (error) {
       return <>Error: { error.message}</>
     } else if (isLoading) {
       return <>Loading...</>
     } else {
+      displayGraph = <Graph weatherData={this.props.weatherData} />
       return (
         <>
           <h1>Weather Data</h1>
           <ul>
             {weatherData.map((weather, index) =>
               <li key={index}>
-
                 <p>{weather.temp.day}</p>
+                <p>{handleDateTimeConversion(weather.dt)}</p>
               </li>
             )}
           </ul>
-
+          {console.log("line 40 of Weather.js", weatherData)}
+          {displayGraph}
         </>
       )
     }
   }
 }
-
-// weatherData["daily"].map(object => object["temp"]["day"])
 
 const mapStateToProps = state => {
   return {
